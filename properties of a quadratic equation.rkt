f315431e918560264d22bade7eeb35e21e827a96
midori <cleoold@gmail.com>
2018-10-15 01:17:36 -0400

    Update properties of a quadratic equation.rkt

--- properties of a quadratic equation.rkt
+++ properties of a quadratic equation.rkt
@@ -3,6 +3,7 @@
 ;; definition of the quadratic
 (define-struct quad (a b c))
 ;; where a: Num, b: Num, c: Num
+;; a is not 0
 
 ;; its solution Soln is
 (define-struct soln (x1 x2))
@@ -32,3 +33,20 @@
 
 ;(soln-quad (make-quad 1 0 4)) yields
 ;(make-soln 0+2i 0-2i)
+
+;; ==================================================
+
+;; computes the vertex of a quadratic function
+
+;; the coordinate of the vertex is (make-posn x y)
+;; symmetry: x
+;; a>0, there is a minimum y; a<0, there is a maximum y
+
+(define (vertex myquad)
+  (make-posn (- (/ (quad-b myquad) (* 2 (quad-a myquad))))
+             (/ (- (* 4 (quad-a myquad) (quad-c myquad)) (sqr (quad-b myquad)))
+                (* 4 (quad-a myquad)))))
+                
+ ;; example
+(vertex (make-quad 6 3.3 4)) yields
+(make-posn -0.275 3.54625)
