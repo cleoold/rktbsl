00500a22b393a10755fd68bcecfb6571d485b1fc
midori <cleoold@gmail.com>
2018-10-15 11:06:04 -0400

    Update solving cubic equations.rkt

--- solving cubic equations.rkt
+++ solving cubic equations.rkt
@@ -6,6 +6,13 @@
 ;; coefficients a, b, c, d are real
 ;; a is not 0
 
+;; its solution soln is
+(define-struct soln (x1 x2 x3))
+;; where x1, x2, x3 are Num
+
 ;; format of a cubic equation
 ;; (make-cubic a b c d) corresponds to
 ;; ax^3+bx^2+cx+d=0
+
+;; trignometric solving method (copied from Wikipedia)
+;; (soln-cubic mycubic): Cubic -> Soln
