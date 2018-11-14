a30ccf8709068a5b68dc6d7888983538c522d2f1
midori <cleoold@gmail.com>
2018-11-14 05:27:12 -0500

    Update math: vectors (racket).rkt

--- math: vectors (racket).rkt
+++ math: vectors (racket).rkt
@@ -35,3 +35,34 @@
 
 (define (vec-mult c u)
   (foldr (lambda (x1 xn) (cons (* x1 c) xn)) '() u))
+  
+;; ..........................................................
+;; inner product
+;; vec-dot: Vec Vec -> Num
+
+(define (vec-dot u v)
+  (foldr (lambda (f1 fn) (+ f1 fn)) 0 (map * u v)))
+  
+;; ..........................................................
+;; cross product
+;; vec-cross: Vec Vec -> Vec
+;; u, v length = 3
+
+(define (vec-cross u v)
+  (list (- (* (cadr u) (caddr v)) (* (caddr u) (cadr v)))
+        (- (* (caddr u) (car v)) (* (car u) (caddr v)))
+        (- (* (car u) (cadr v)) (* (cadr u) (car v)))))
+        
+;; ==========================================================
+;; two vectors shall be same length
+
+;; projection of vector x onto vector u
+;; (also perpendicular of vector x onto plane with normal n)
+;; proj: Vec Vec -> Vec
+
+(define (proj u x)
+  (foldr
+   (lambda (x1 xn)
+     (cons (* x1 (/ (foldr (lambda (f1 fn) (+ f1 fn)) 0 (map * x u))
+                    (foldr (lambda (x1 xn) (+ (sqr x1) xn)) 0 u))) xn))
+   '() u))
