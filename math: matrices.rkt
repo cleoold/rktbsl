8c7e49c5d80b9b776a0c5c621abf5067e834d370
midori <cleoold@gmail.com>
2018-11-26 18:44:28 -0500

    Update math: matrices.rkt

--- math: matrices.rkt
+++ math: matrices.rkt
@@ -3,6 +3,8 @@
 ;; a matrix is really a series of rows/columns of vectors so
 ;; refer to "rktbsl/math: vectors.rkt" for required functions
 
+;(require "math: vectors (racket).rkt")
+
 ;; defining a matrix
 ;; a matrix (Mtx) is a list
 ;; (cons (list of Num) null)
