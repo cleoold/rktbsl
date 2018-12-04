5446323daa3e241a26217fdd709477d116e8368a
midori <cleoold@gmail.com>
2018-12-03 22:06:00 -0500

    Update my fibonacci.rkt

--- my fibonacci.rkt
+++ my fibonacci.rkt
@@ -46,6 +46,8 @@
              [else (fib-adder b (+ a b) (add1 counter))]))]
        (fib-adder 0 1 2))]))
        
+; (build-list 20 fibonacci4) -> '(0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181)
+       
 ;; ..................................................................
 
 ;; with offsets and weights
@@ -53,13 +55,30 @@
 ;; F(1) = B
 ;; F(n) = xF(n-2) + yF(n-1)
 
-(define (weighted-fibonacci i A B x y)
+(define (weighted-fibonacci n A B x y)
   (cond
-    [(= i 0) A]
-    [(= i 1) B]
+    [(= n 0) A]
+    [(= n 1) B]
     [else
      (local
        [(define (fib-adder a b counter)
-       (cond [(= i counter) (+ (* x a) (* y b))]
+       (cond [(= n counter) (+ (* x a) (* y b))]
              [else (fib-adder b (+ (* x a) (* y b)) (add1 counter))]))]
        (fib-adder A B 2))]))
+       
+; (build-list 20
+;   (lambda (n) (weighted-fibonacci n 4 6 2 -1)))
+; -> '(4 6 2 10 -6 26 -38 90 -166 346 -678 1370 -2726 5466 -10918 21850 -43686 87386 -174758 349530)
+       
+;; ..................................................................
+
+;; subsequence: (listof Any) Nat Nat -> (listof Any)
+
+(define (subsequence lst from to)
+  (foldr (lambda (l1 j rr)
+           (if (and (>= j from) (< j to))
+               (cons l1 rr)
+               rr))
+         null lst (build-list (length lst) identity)))
+         
+; (subsequence '(0 1 2 3 4 5 6) 2 4) -> '(2 3)
