aa5f72ea0ea496b991dcc65ce38d63d858a040ad
midori <cleoold@gmail.com>
2018-12-05 19:34:04 -0500

    Update math: binary.rkt

--- math: binary.rkt
+++ math: binary.rkt
@@ -1,21 +1,27 @@
 #lang racket
 
-;; something about base-n conversion
+;; something about base-n conversion and applications in binary conversion
 
-;; converts a base-n integer to a decimal integer
-;; basen->dec: Num Nat -> Num
+(require "int and list.rkt")
+
+;; converts a base-n integer to a decimal "integer" (there are probably fraction base)
+;; basen->dec: Num Int -> Num
 ;; requires: nbase > 0
 
-;; [exponential expansion]
+;; [regular exponential expansion]
 (define (basen->dec2 nbase nnumber)
   (local
-    [(define numlst (integer->list nnumber))
-     (define degree (length numlst))]
-    (foldr (lambda (numl1 degl1 rr)
-             (+ (* numl1 (expt nbase degl1)) rr))
-           0
-           numlst
-           (reverse (build-list degree identity)))))
+    [(define numlst (integer->intlist (abs nnumber)))
+     (define degree (length numlst))
+     (define (if-positive degreel numlst)
+       (foldr (lambda (numl1 degl1 rr)
+                (+ (* numl1 (expt nbase degl1)) rr))
+              0
+              numlst
+              (reverse (build-list degree identity))))]
+    (if (>= nnumber 0)
+        (if-positive degree numlst)
+        (- (if-positive degree numlst)))))
            
 ;; [faster expansion]
 (define (basen->dec nbase nnumber)
@@ -23,3 +29,18 @@
       nnumber
       (+ (modulo nnumber 10) 
          (* nbase (basen->dec nbase (quotient nnumber 10))))))
+         
+         
+;; converts a decimal integer to a base-n integer
+;; basen->dec: Nat Int -> Int
+
+(define (dec->basen nbase nnumber)
+  (local
+    [(define (dec-basen/reversed nnumber)
+       (if (zero? nnumber)
+           null
+           (cons (modulo nnumber nbase)
+                 (dec-basen/reversed (quotient nnumber nbase)))))]
+    (reverse (dec-basen/reversed nnumber))))
+    
+;; ======================================================================
