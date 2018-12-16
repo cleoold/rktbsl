99cda8a829dbe248350e2d72ecf0807baba0a2ab
midori <cleoold@gmail.com>
2018-12-15 23:21:25 -0500

    Update math: my fibonacci.rkt

--- math: my fibonacci.rkt
+++ math: my fibonacci.rkt
@@ -70,15 +70,3 @@
 ;   (lambda (n) (weighted-fibonacci n 4 6 2 -1)))
 ; -> '(4 6 2 10 -6 26 -38 90 -166 346 -678 1370 -2726 5466 -10918 21850 -43686 87386 -174758 349530)
        
-;; ..................................................................
-
-;; subsequence: (listof Any) Nat Nat -> (listof Any)
-
-(define (subsequence lst from to)
-  (foldr (lambda (l1 j rr)
-           (if (and (>= j from) (< j to))
-               (cons l1 rr)
-               rr))
-         null lst (build-list (length lst) identity)))
-         
-; (subsequence '(0 1 2 3 4 5 6) 2 4) -> '(2 3)
