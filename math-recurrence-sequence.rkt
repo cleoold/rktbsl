769a59bff854435622161ca6cc56be8d48095ef0
midori <cleoold@gmail.com>
2019-05-15 18:14:50 +0800

    Update math-recurrence-sequence.rkt

--- math-recurrence-sequence.rkt
+++ math-recurrence-sequence.rkt
@@ -6,9 +6,9 @@
 
 ;; with one offset and one term for recursion eg:
 ;; a(0) = A
-;; a(n) = f(n-1)
+;; a(n) = f(a[n-1], n)
 
-;; recursive-sequence1: X (X -> X) -> (Nat -> X)
+;; recursive-sequence1: X (X Nat -> X) -> (Nat -> X)
 
 (define (recursive-sequence1 A f)
   (lambda (n)
@@ -17,21 +17,21 @@
       [else
        (local
          [(define (adder a counter)
-            (cond [(= n counter) (f a)]
-                  [else (adder (f a) (add1 counter))]))]
+            (cond [(= n counter) (f a counter)]
+                  [else (adder (f a counter) (add1 counter))]))]
          (adder A 1))])))
          
 ;; example
 ;{ a(0)=1, a(n)=2*a(n-1) }  first 10 terms
-;(build-list 10 (recursive-sequence1 1 (λ (x) (* 2 x)))) -> '(1 2 4 8 16 32 64 128 256 512)
+;(build-list 10 (recursive-sequence1 1 (λ (x n) (* 2 x)))) -> '(1 2 4 8 16 32 64 128 256 512)
 
 
 ;; with two offsets and two terms for recursion eg:
 ;; a(0) = A
 ;; a(1) = B
-;; a(n) = f(n-2, n-1)
+;; a(n) = f(a[n-2], a[n-1], n)
 
-;; recursive-sequence2: X X (X X -> X) -> (Nat -> X)
+;; recursive-sequence2: X X (X X Nat -> X) -> (Nat -> X)
 
 (define (recursive-sequence2 A B f)
   (lambda (n)
@@ -41,13 +41,13 @@
       [else
        (local
          [(define (adder a b counter)
-            (cond [(= n counter) (f a b)]
-                  [else (adder b (f a b) (add1 counter))]))]
+            (cond [(= n counter) (f a b counter)]
+                  [else (adder b (f a b counter) (add1 counter))]))]
          (adder A B 2))])))
          
 ;; example
 ;{ a(0)=1, a(1)=1 a(n)=a(n-2)+a(n-1)^2 } first 7 terms
-;(build-list 7 (recursive-sequence2 1 1 (λ (x y) (+ x (sqr y))))) -> '(1 1 2 5 27 734 538783)
+;(build-list 7 (recursive-sequence2 1 1 (λ (x y n) (+ x (sqr y))))) -> '(1 1 2 5 27 734 538783)
 
 ;; ................................................................................
 ;; solve equations f=0 with Newton's method
@@ -59,7 +59,7 @@
 
 (define (newton-solve f guess step)
   (recursive-sequence1 guess
-    (lambda (x) (- x (/ (f x) (right-derivative f x step))))))
+    (lambda (x n) (- x (/ (f x) (right-derivative f x step))))))
 
 ;; example
 ;solve cos(x)-x=0
